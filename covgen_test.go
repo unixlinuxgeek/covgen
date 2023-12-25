6d4442d0067116da9a0bbe4fbeeb0a0b23ce161e
unixlinuxgeek <unixlinuxgeek@gmail.com>
2023-12-26 02:25:47 +0500

    Added tests

--- covgen_test.go
+++ covgen_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"testing"
 )
 
 func TestGenerate(t *testing.T) {
-	err := generate("./test_data/in.mp4", "./out11.jpg")
+	err := generate("./test_data/in.mp4", "/tmp/out_"+fmt.Sprint(rand.Int())+".jpg")
 	if err != nil {
 		log.Fatal(err)
 	}
